103f18b4eb8f81e476a8f62e8875bdaa53251ef1
rreichelt21 <rreichelt.2002@gmail.com>
2022-09-15 01:38:38 -0700

    Update FinanceApp.py

--- FinanceApp.py
+++ FinanceApp.py
@@ -1,8 +1,10 @@
 import tkinter as tk 
 def create():
     win = tk.Toplevel(root)
+
 root = tk.Tk()
+root.wm_title("Finance App")
 root.geometry('500x200')  
-btn = tk.Button(root, text="Create a new window", command = create)
+btn = tk.Button(root, text="Create an account!", command = create)
 btn.pack(pady = 10) 
 root.mainloop()
