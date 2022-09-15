75514b29e67ab5077241ad6d368fc377e8c1e92b
rreichelt21 <rreichelt.2002@gmail.com>
2022-09-15 00:35:53 -0700

    Update

--- Creditcards.py
+++ Creditcards.py
@@ -1,2 +1,5 @@
-import math
-import matplotlib.pyplot as plt
+import tkinter as tk
+
+def CreditCards():
+    Label = tk.Label(text="Welcome to the Finance App",
+                bg="black", fg="white", font="none 12 bold")
--- FinanceApp.py
+++ FinanceApp.py
@@ -1,82 +1,24 @@
-#THE GUI FOR FINANCE APP
+import tkinter as tk
+import Creditcards
 
-import PySimpleGUI as sg
-sg.theme('Black')
+app = tk.Tk()
+app.title("Finance App")
+app.configure(bg = "black", width = 500, height = 500)
 
-#MAIN LAYOUT
+Label = tk.Label(text="Welcome to the Finance App",
+                bg="black", fg="white", font="none 12 bold")
 
-def mainwindow():
-    MainLayout = [[sg.Text('Welcome to the Finance App!')],
-                  [sg.Button('Credit Cards'), sg.Button('Finance Tracking'), sg.Button('Account Balance'), sg.Button('Exit')]]
-    
-    return sg.Window('Finance App', MainLayout, finalize=True)
+Button1 = tk.Button(text="Credit Cards", bg = "white", fg = "black", 
+                    font = "none 15 bold", width = 10, height = 1)                
 
-#SUB LAYOUTS INSIDE MAIN LAYOUT
+# Button2 = tk.Button(text="Account Balance", bg = "white", fg = "black", 
+#                     font = "none 15 bold", width = 15, height = 1)
 
-def CreditCards():
-    CCLayout = [[sg.Text ('Credit Cards')],
-                [sg.InputText()],
-                [sg.Button('Back')]]
-    
-    return sg.Window('Credit Cards', CCLayout, finalize=True)
+# Button3 = tk.Button(text="Finance Tracking", bg = "white", fg = "black", 
+#                     font = "none 15 bold", width = 15, height = 1)
 
-def FinanceTracker():
-    FTLayout = [[sg.Text ('Finance Tracking')],
-                [sg.InputText()],
-                [sg.Button('Back')]]
-    
-    return sg.Window('Finance Tracking', FTLayout, finalize=True)
-
-def AccountBalance():
-    ABLayout = [[sg.Text ('Account Balance')],
-                [sg.InputText()],
-                [sg.Button('Back')]]
-
-    return sg.Window('Account Balance', ABLayout, finalize=True)
-
-#WINDOWS
-
-window1 = mainwindow()
-
-#MAIN LOOP
-while True:
-    event, values = window1.read()
-    
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-
-    if event == 'Credit Cards':
-        window1.hide()
-        window2 = CreditCards()
-
-        while True:
-            event, values = window2.read()
-            
-            if event == 'Back':
-                window2.hide()
-                window1.un_hide()
-                break
-
-    if event == 'Finance Tracking':
-        window1.hide()
-        window3 = FinanceTracker()
-
-        while True:
-            event, values = window3.read()
-            
-            if event == 'Back':
-                window3.hide()
-                window1.un_hide()
-                break
-
-    if event == 'Account Balance':
-        window1.hide()
-        window4 = AccountBalance()
-
-        while True:
-            event, values = window4.read()
-            
-            if event == 'Back':
-                window4.hide()
-                window1.un_hide()
-                break
+Label.pack()
+Button1.pack()
+# Button2.pack()
+# Button3.pack()
+app.mainloop()
