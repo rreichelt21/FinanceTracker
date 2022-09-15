2b3ace8bab40db9009b30b5123bd32f0acdbeaa0
rreichelt21 <rreichelt.2002@gmail.com>
2022-09-15 01:20:13 -0700

    Update

--- FinanceApp.py
+++ FinanceApp.py
@@ -1,24 +1,8 @@
-import tkinter as tk
-import Creditcards
-
-app = tk.Tk()
-app.title("Finance App")
-app.configure(bg = "black", width = 500, height = 500)
-
-Label = tk.Label(text="Welcome to the Finance App",
-                bg="black", fg="white", font="none 12 bold")
-
-Button1 = tk.Button(text="Credit Cards", bg = "white", fg = "black", 
-                    font = "none 15 bold", width = 10, height = 1)                
-
-# Button2 = tk.Button(text="Account Balance", bg = "white", fg = "black", 
-#                     font = "none 15 bold", width = 15, height = 1)
-
-# Button3 = tk.Button(text="Finance Tracking", bg = "white", fg = "black", 
-#                     font = "none 15 bold", width = 15, height = 1)
-
-Label.pack()
-Button1.pack()
-# Button2.pack()
-# Button3.pack()
-app.mainloop()
+import tkinter as tk 
+def create():
+    win = tk.Toplevel(root)
+root = tk.Tk()
+root.geometry('500x200')  
+btn = tk.Button(root, text="Create a new window", command = create)
+btn.pack(pady = 10) 
+root.mainloop()
--- test.py
+++ test.py
@@ -1,28 +1,8 @@
-from tkinter import Tk, Label, Button
-
-
-class QBox:
-    """ Class that is an object"""
-
-    def __init__(self, master):
-        """ Class init Method"""
-        self.master = master
-        master.title("Question Box")
-        self.label = Label(master, text="Welcome to the Question Box")
-        self.button = Button(master, command=self.change, text="Clickme", fg='Black')
-
-        self.label.pack()
-        self.button.pack()
-
-    def change(self):
-        self.label.pack_forget()
-
-
-def main():
-    root = Tk()
-    QBox(root)
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+from tkinter import *   
+def create():
+    win = Toplevel(root)
+root = Tk()
+root.geometry('200x100')  
+btn = Button(root, text="Create a new window", command = create)
+btn.pack(pady = 10) 
+root.mainloop()
